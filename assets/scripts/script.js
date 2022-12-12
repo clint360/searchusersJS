5b5e762156f19cd70aad9b3546166ddada85a686
clint360 <clint360.rebase@gmail.com>
2022-12-12 13:29:07 +0100

    created the JS files and coded the logic behind

---
+++ assets/scripts/script.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('form');
+const usersContainer = document.querySelector('#results')
+
+function abbr(name) {
+ let str = name;
+ stringArray = str.split(/(\s+)/);
+ return `${stringArray[0][0]}.${stringArray[2][0]}`;
+}
+
+function displayUser({name, age}) {
+  return `
+  <div class="searchresultcontainer">
+  <div class="circle">${abbr(name)}</div>
+  <div class="name-age">
+    <h4>${name}</h4>
+    <p>${age} year${age > 1? "s" : ""} old</p>
+  </div>
+</div>
+  `
+}
+
+function displayUsers(persons) {
+  return persons.map(displayUser).join("")
+}
+
+function searchUsers (name, age) {
+  const result = [];
+  for(let i = 0; i < users.length; i++) {
+    let isIncluded = true;
+    let user = users[i]
+    if(name && user.name !== name) {
+      isIncluded = false;
+    }
+    if (age && user.age !==age) {
+       isIncluded = false;
+    }
+    if(isIncluded) {
+      result.push(user);
+    }
+  }
+  return result;
+}
+
+usersContainer.innerHTML = displayUsers(users);
---
+++ assets/scripts/users.js
@@ -0,0 +1,16 @@
+const users = [
+  {name: 'Chia Clint', age: 17},
+  {name: 'Gaston Che', age: 26},
+  {name: 'Chriss Godden', age:23},
+  {name: 'Alaric Mozzart', age: 18},
+  {name: 'Chia Carlyle', age: 26},
+  {name: 'Chia Patience', age: 51},
+  {name: 'Chia Kenneth', age: 63},
+  {name: 'Sparkles Emmy', age: 17},
+  {name: 'Batis Emile', age: 26},
+  {name: 'Christelle Pearl', age:23},
+  {name: 'Nazar Vinitius', age: 18},
+  {name: 'Chia Clive', age: 23},
+  {name: 'Chia Victor', age: 51},
+  {name: 'Nchin Leonard', age: 63},
+]
